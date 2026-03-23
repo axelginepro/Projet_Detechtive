1e395fbaa5cd51debb41e20d34886ab82a284750
Axel GINEPRO <44256610+axelginepro@users.noreply.github.com>
2026-03-23 09:33:19 +0100

    Update config.php

--- webapp php/config.php
+++ webapp php/config.php
@@ -8,12 +8,12 @@ if (file_exists($lib_path)) {
 }
 
 // --- CONFIGURATION DE L'APPLICATION ---
-define('DB_SERVER', '192.168.10.18');
+define('DB_SERVER', '192.168.10.21');
 define('DB_USERNAME', 'db_connect'); 
 define('DB_NAME', 'detechtive_db');
 
 // --- CONFIGURATION FILE SERVER ---
-define('FS_IP', '192.168.10.19'); 
+define('FS_IP', '192.168.10.20'); 
 define('FS_SHARE_NAME', 'Detechtive'); 
 define('FS_USER', 'DETECHTIVE\\Administrator'); 
 ?>
